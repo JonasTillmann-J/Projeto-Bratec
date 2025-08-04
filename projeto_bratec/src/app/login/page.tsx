f3ceb4a6649b859135813cf42d5d667b2d6d6d2b
Tillmann.J <139597953+JonasTillmann-J@users.noreply.github.com>
2025-08-04 19:04:51 +0000

    criação da estrutura e dos elementos das paginas

--- projeto_bratec/src/app/login/page.tsx
+++ projeto_bratec/src/app/login/page.tsx
@@ -2,10 +2,7 @@ export default function Login() {
   return (
     <section className="flex flex-col items-center justify-center gap-4">
       <h2 className="text-3xl font-semibold">Bem-vindo ao sistema Bratec</h2>
-      <p className="text-gray-300 text-center max-w-xl">
-        essa é a tela de login do sistema Bratec, onde você pode acessar as funcionalidades do sistema. Para continuar, por favor, faça o login com suas credenciais.
-      </p>
-      <Link></Link>
+    
     </section>
   )
 }
--- projeto_bratec/src/app/page.tsx
+++ projeto_bratec/src/app/page.tsx
@@ -1,17 +1,12 @@
 import Link from 'next/link'
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4">
-      <h2 className="text-3xl font-semibold">Bem-vindo ao sistema Bratec</h2>
-      <p className="text-gray-300 text-center max-w-xl">
-        Esta é a versão web para gestão, controle e suporte técnico de impressoras e dispositivos de automação comercial.
-      </p>
-      <Link
-        href="/login"
-        /*bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded text-white shadow*/
-        className="bg-red-700 hover:bg-blue-900 hover:text-black transition ease-in-out duration-400 px-15 py-5">
-        Ir para Login
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">Bem-vindo à Bratec Assistência Técnica</h1>
+      <p className="mb-4">Reparo profissional de celulares em Blumenau‑SC.</p>
+      <Link href="/servicos" className="text-pink-500 hover:underline">
+        Confira nossos serviços
       </Link>
-    </section>
-  )
+    </div>
+  ) 
 }
