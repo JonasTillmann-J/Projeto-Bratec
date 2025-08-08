fb490fc8dc59b0c441772c48dcf9bd15b6bd12d9
Tillmann.J <139597953+JonasTillmann-J@users.noreply.github.com>
2025-08-08 11:06:17 +0000

    Construção do Index

--- projeto_bratec/src/app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Login() {
-  return (
-    <section className="flex flex-col items-center justify-center gap-4">
-      <h2 className="text-3xl font-semibold">Bem-vindo ao sistema Bratec</h2>
-    
-    </section>
-  )
-}
